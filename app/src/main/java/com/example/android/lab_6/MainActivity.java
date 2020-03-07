059da2e54599d802778e22ab2af8ce980b2ecd34
akun <aliceliyukun@outlook.com>
2020-03-07 00:26:07 -0600

    -m"need to add in madison bascom hall"

--- app/src/main/java/com/example/android/lab_6/MainActivity.java
+++ app/src/main/java/com/example/android/lab_6/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.android.lab_6;
 
-import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.fragment.app.FragmentActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.MarkerOptions;
+public class MainActivity extends FragmentActivity {
+    private GoogleMap mMap;
+    //private FusedLocationProviderClient mFusedLocationProviderClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_map);
+                getSupportFragmentManager().findFragmentById(R.id.fragment_map);
+        mapFragment.getMapAsync(googleMap->{
+            mMap = googleMap;
+            googleMap.addMarker(new MarkerOptions().position(mDestinationLatLng).title("Bascom Hall"));
+
+        });
     }
 }
