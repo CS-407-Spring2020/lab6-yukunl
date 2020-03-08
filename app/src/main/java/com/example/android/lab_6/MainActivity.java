ef27bce76dfb4db1d2db593112646d972366ea5d
akun <aliceliyukun@outlook.com>
2020-03-07 21:40:10 -0600

    -m"Finished? but shows in california "

--- app/src/main/java/com/example/android/lab_6/MainActivity.java
+++ app/src/main/java/com/example/android/lab_6/MainActivity.java
@@ -1,19 +1,26 @@
 package com.example.android.lab_6;
 
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
 import android.os.Bundle;
-//import android.location.Location;
-//import com.google.android.gms.location.FusedLocationProviderClient;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MainActivity extends FragmentActivity {
     private final LatLng mDestinationLatLng = new LatLng(43.0754,-89.4042);
     private GoogleMap mMap;
-   // private FusedLocationProviderClient mFusedLocationProviderClient;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 12;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -25,7 +32,29 @@ public class MainActivity extends FragmentActivity {
             googleMap.addMarker(new MarkerOptions().position(mDestinationLatLng).title("Bascom Hall"));
 
         });
-    //   mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+       mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        displayMyLocation();
+    }
 
+    private void displayMyLocation() {
+        int permission = ActivityCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permission == PackageManager.PERMISSION_DENIED)
+        {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+        else
+        {
+            mFusedLocationProviderClient.getLastLocation()
+                    .addOnCompleteListener(this, task -> {
+                        Location mLastKnownLocation = task.getResult();
+                        if (task.isSuccessful() && mLastKnownLocation != null)
+                        {
+                            LatLng currentLatLng = new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude());
+                            mMap.addMarker(new MarkerOptions().position(currentLatLng).title("current location"));
+                            mMap.addPolyline(new PolylineOptions().add(
+                                    new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude()), mDestinationLatLng));
+                        }
+                    });
+        }
     }
 }
